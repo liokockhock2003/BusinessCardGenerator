5a69d2296d7551d7b8a6380d757b9feaeccfe45f
HisyamFirdaus03 <hisyamfirdaus100@gmail.com>
2024-06-25 01:09:26 +0800

    card_id

--- scripts/updateCardData.js
+++ scripts/updateCardData.js
@@ -1,6 +1,6 @@
 
 export function updateCardDetails(id){
-    document.getElementById('templateID').value = id;
+    document.getElementById('card_id').value = id;
     document.getElementById('update-overlay').style.display = 'flex';
     document.getElementById(id).onclick = null;
 }
