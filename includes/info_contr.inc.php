9b99de71e34c7656949eb2c8d4ba0e5b11764ae8
HisyamFirdaus03 <hisyamfirdaus100@gmail.com>
2024-06-24 14:18:54 +0800

    Updated

--- includes/info_contr.inc.php
+++ includes/info_contr.inc.php
@@ -2,8 +2,8 @@
 
 declare(strict_types=1);
 
-function is_info_empty(string $name, string $position, string $email, string $address, string $template_id,$imgData){
-    if(empty($name) || empty($position) || empty($email) || empty($address) || empty($template_id) ||empty($imgData)){
+function is_info_empty(string $name, string $position, string $email, string $address, string $template_id, $imgData){
+    if(empty($name) || empty($position) || empty($email) || empty($address) || empty($template_id) || empty($imgData)){
         return true;
     } else {
         return false;
