220db63bc9e33a9d8913092238b73608e0bce05e
Saifdn <saifudinahmad1234@gmail.com>
2024-06-24 19:06:25 +0800

    Update overlay

--- profilepage.php
+++ profilepage.php
@@ -71,25 +71,21 @@
 
         <!-- Overlay Upate Form -->
         <div id="update-overlay" class="update-overlay">
-            <h1>Business Cards</h1>
-            <p>Update new information about your card!</p>
-            <form action="includes/info.inc.php" method="post" class="formsize" id="signupForm" enctype="multipart/form-data">
-                <input type="text" name="name" id="name" class="form-control" placeholder="Your Name"> <br>
-                <input type="text" name="position" id="position" class="form-control" placeholder="Your Position"> <br>
-                <input type="text" name="email" id="email" class="form-control" placeholder="Your Email"> <br>
-                <input type="text" name="address" id="address" class="form-control" placeholder="Your Address"> <br>
-                <input type="file" name="image" id="upload" class="form-control-file"  aria-describedby="fileHelp">
-                <label for="upload"> <i class="fa fa-cloud-upload"></i></i>Upload Picture</label><br><br>
+            <div class="warp">
+                <h1>Business Cards</h1>
+                <p>Update new information about your card!</p>
+                <form action="includes/info.inc.php" method="post" class="formsize" id="signupForm" enctype="multipart/form-data">
+                    <input type="text" name="name" id="name" class="form-control" placeholder="Your Name"> <br>
+                    <input type="text" name="position" id="position" class="form-control" placeholder="Your Position"> <br>
+                    <input type="text" name="email" id="email" class="form-control" placeholder="Your Email"> <br>
+                    <input type="text" name="address" id="address" class="form-control" placeholder="Your Address"> <br>
+                    <input type="file" name="image" id="upload" class="form-control-file"  aria-describedby="fileHelp">
+                    <label for="upload"> <i class="fa fa-cloud-upload"></i></i>Upload Picture</label><br><br>
 
-                <div class="select-container">
-                    <div class = "icon-container">
-                        <i class="fa fa-caret-down"></i>
-                    </div>
-                </div>
-            <br>
-                        
-            <input type="submit" name="submit" value="Update Now" class="btn btn-primary"> <br><br>
-            </form>
+                    <input type="submit" name="cancel" value="Cancel" class="btn btn-cancel">
+                    <input type="submit" name="submit" value="Update Now" class="btn btn-primary"> <br><br>
+                </form>
+            </div>    
         </div>
 
         <!--footer-->
