73b922757eaf2696b65aca98b9912bd0bd8970cf
HisyamFirdaus03 <hisyamfirdaus100@gmail.com>
2024-06-24 14:18:35 +0800

    Added count file to retrieve JSON file to JavaScript

---
+++ includes/getInfo_count.inc.php
@@ -0,0 +1,35 @@
+<?php
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+require_once 'dbh.inc.php'; 
+require_once 'config_session.inc.php';
+require_once 'getInfo.inc.php';
+header('Content-Type: application/json');
+    
+if (!isset($_SESSION["user_id"])) {
+    echo json_encode(["error" => "User ID not set in session"]);
+    exit;
+}
+
+try {
+    // Retrieve the number of records from SELECT statement
+    if ($cardDetails === false) {
+        throw new Exception("Failed to fetch card count");
+    }
+
+    $data = [];
+    $index = 0;
+    foreach($cardDetails as $card){
+        $data["id_".$index] = $card['template_id'];
+        $index++;
+    }
+
+    echo json_encode($data);
+} catch (Exception $e) {
+    // Catch any exceptions and output as JSON error message
+    echo json_encode(["error" => $e->getMessage()]);
+}
+
+
