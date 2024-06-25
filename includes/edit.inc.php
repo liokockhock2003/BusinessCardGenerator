524b2ad4189370dc9dc96562f3419f91436d42b9
liokockhock2003 <liokockhock@gmail.com>
2024-06-25 10:50:05 +0800

    fix edit error

--- includes/edit.inc.php
+++ includes/edit.inc.php
@@ -16,47 +16,44 @@ if($_SERVER["REQUEST_METHOD"] === "POST"){
                 
                 $card_id = $card['card_id'];
 
-                if (isset($_POST["name"])) {
+                if (isset($_POST["name"]) && !empty($_POST["name"])) {
                     $name = $_POST["name"];
-                } else{
+                } else if(isset($card["name"])){
                     $name = $card['name'];
                 }
 
-                if (isset($_POST["position"])) {
+                if (isset($_POST["position"]) && !empty($_POST["position"])) {
                     $position = $_POST["position"];
-                } else{
+                } else if(isset($card["position"])){
                     $position = $card["position"];
                 }
 
-                if (isset($_POST["email"])) {
+                if (isset($_POST["email"]) && !empty($_POST["email"])) {
                     $email = $_POST["email"];
-                } else{
+                } else if(isset($card["email"])){
                     $email = $card['email'];
                 }
 
-                if (isset($_POST["address"])) {
+                if (isset($_POST["address"]) && !empty($_POST["address"])) {
                     $address = $_POST["address"];
-                } else{
+                } else if(isset($card["address"])){
                     $address = $card["address"];
                 }
 
-                if (isset($_POST["template_id"])) {
+                if (isset($_POST["template_id"]) && !empty($_POST["template_id"])) {
                     $template_id = $_POST["template_id"];
-                } else{
+                } else if(isset($card["template_id"])){
                     $template_id = $card["template_id"];
                 }
 
-                if ( $_FILES['image']['name'] !== null) {
+                if ( $_FILES['image']['name'] !== null && !empty($_FILES["image"]['name'])) {
                     $imgData = $_FILES['image']['name'];
                     $tempFilePath = $_FILES['image']['tmp_name'];
                     $imgData = "../image/".$imgData;
                     move_uploaded_file($tempFilePath, $imgData);
                     
-                } else {
+                } else if(isset($card["image"])) {
                     $imgData = $card['image'];
-                    $tempFilePath = $_FILES['image']['tmp_name'];
-                    $imgData = "../image/".$imgData;
-                    move_uploaded_file($tempFilePath, $imgData);
                 }
 
             }
